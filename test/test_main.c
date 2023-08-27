87dc69a1bcd14557930c5bdf7ac5edf951d7f8e4
Afif Ramadhan <ramadhanafif@gmail.com>
2023-08-27 18:25:59 +0700

    test: add test for insert method and shrink method

--- test/test_main.c
+++ test/test_main.c
@@ -78,10 +78,97 @@ void test_vector_remove(void)
     vector_destroy(vector);
 }
 
+void test_insert()
+{
+    int *vector = vector_create();
+    vector      = vector_push_back(vector, 0);
+    vector      = vector_push_back(vector, 1);
+    vector      = vector_push_back(vector, 2);
+    vector      = vector_push_back(vector, 3);
+    vector      = vector_push_back(vector, 4);
+    vector      = vector_push_back(vector, 5);
+    vector      = vector_push_back(vector, 6);
+    vector      = vector_push_back(vector, 7);
+    vector      = vector_push_back(vector, 8);
+    vector      = vector_push_back(vector, 9);
+
+    TEST_ASSERT_EQUAL(10, vector_size(vector));
+    TEST_ASSERT_EQUAL(10, vector_len(vector));
+
+    vector_insert(vector, 0, 10);
+    TEST_ASSERT_EQUAL(11, vector_size(vector));
+    TEST_ASSERT_EQUAL(11, vector_len(vector));
+    int exp1[] = { 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    TEST_ASSERT_EQUAL_INT_ARRAY(exp1, vector, 11);
+
+    vector_insert(vector, 4, 11);
+    TEST_ASSERT_EQUAL(12, vector_size(vector));
+    TEST_ASSERT_EQUAL(12, vector_len(vector));
+    int exp2[] = { 10, 0, 1, 2, 11, 3, 4, 5, 6, 7, 8, 9 };
+    TEST_ASSERT_EQUAL_INT_ARRAY(exp2, vector, 12);
+
+    vector_insert(vector, 7, 12);
+    TEST_ASSERT_EQUAL(13, vector_size(vector));
+    TEST_ASSERT_EQUAL(13, vector_len(vector));
+    int exp3[] = { 10, 0, 1, 2, 11, 3, 4, 12, 5, 6, 7, 8, 9 };
+    TEST_ASSERT_EQUAL_INT_ARRAY(exp3, vector, 13);
+
+    vector_destroy(vector);
+}
+
+void test_shrink()
+{
+    /**
+     * This test aims to test the shrink function
+     * 1. Create a vector with 10 elements
+     * 2. Remove 2 elements using vector_remove, assert the length and size
+     * 3. Shrink the vector, assert the length and size
+     * 4. Remove 2 elements using vector_pop_back, assert the length and size
+     * 5. Shrink the vector, assert the length and size
+     */
+
+    int *vector = vector_create();
+    vector      = vector_push_back(vector, 0);
+    vector      = vector_push_back(vector, 1);
+    vector      = vector_push_back(vector, 2);
+    vector      = vector_push_back(vector, 3);
+    vector      = vector_push_back(vector, 4);
+    vector      = vector_push_back(vector, 5);
+    vector      = vector_push_back(vector, 6);
+    vector      = vector_push_back(vector, 7);
+    vector      = vector_push_back(vector, 8);
+    vector      = vector_push_back(vector, 9);
+
+    TEST_ASSERT_EQUAL(10, vector_size(vector));
+    TEST_ASSERT_EQUAL(10, vector_len(vector));
+
+    vector_remove(vector, 0);
+    vector_remove(vector, 5);
+    TEST_ASSERT_EQUAL(10, vector_size(vector));
+    TEST_ASSERT_EQUAL(8, vector_len(vector));
+
+    vector_shrink_to_fit(vector);
+    TEST_ASSERT_EQUAL(8, vector_size(vector));
+    TEST_ASSERT_EQUAL(8, vector_len(vector));
+
+    vector_pop_back(vector);
+    vector_pop_back(vector);
+    TEST_ASSERT_EQUAL(8, vector_size(vector));
+    TEST_ASSERT_EQUAL(6, vector_len(vector));
+
+    vector_shrink_to_fit(vector);
+    TEST_ASSERT_EQUAL(6, vector_size(vector));
+    TEST_ASSERT_EQUAL(6, vector_len(vector));
+
+    vector_destroy(vector);
+}
+
 test_runner_t *test_runners[] = {
-    test_simple_pushback,
-    test_pop_back,
-    test_vector_remove,
+    // test_simple_pushback,
+    // test_pop_back,
+    // test_vector_remove,
+    // test_insert,
+    test_shrink,
 };
 
 size_t test_runners_len = sizeof(test_runners) / sizeof(test_runners[0]);
